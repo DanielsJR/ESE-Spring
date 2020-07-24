d3699876374ea5dd121dff3e64c4f9535502d862
Daniels <d.rubio@outlook.com>
2020-07-24 17:28:36 -0400

    add environment to DbSeeder

--- src/main/java/nx/ese/services/data/DatabaseSeederService.java
+++ src/main/java/nx/ese/services/data/DatabaseSeederService.java
@@ -2,6 +2,7 @@ package nx.ese.services.data;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
@@ -39,145 +41,152 @@ import nx.ese.utils.NX_UtilBase64Image;
 @Service
 public class DatabaseSeederService {
 
-	@Value("${nx.admin.username}")
-	private String username;
+    @Value("${nx.admin.username}")
+    private String username;
 
-	@Value("${nx.admin.password}")
-	private String password;
+    @Value("${nx.admin.password}")
+    private String password;
 
-	@Value("${nx.databaseSeeder.ymlFileName:#{null}}")
-	private Optional<String> ymlFileName;
+    @Value("${nx.databaseSeeder.ymlFileName:#{null}}")
+    private Optional<String> ymlFileName;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private PreferencesRepository preferencesRepository;
+    @Autowired
+    private PreferencesRepository preferencesRepository;
 
-	@Autowired
-	private CourseRepository courseRepository;
+    @Autowired
+    private CourseRepository courseRepository;
 
-	@Autowired
-	private SubjectRepository subjectRepository;
-	
-	@Autowired
-	private EvaluationRepository evaluationRepository;
-	
-	@Autowired
-	private GradeRepository gradeRepository;
-	
-	@Autowired
-	private QuizRepository quizRepository;
-	
-	@Autowired
-	private QuizStudentRepository quizStudentRepository;
-	
-	@Autowired
-	private AttendanceRepository attendanceRepository;
+    @Autowired
+    private SubjectRepository subjectRepository;
 
-	@Autowired
-	private MongoTemplate mongoTemplate;
+    @Autowired
+    private EvaluationRepository evaluationRepository;
 
-	private static final Logger logger = LoggerFactory.getLogger(DatabaseSeederService.class);
+    @Autowired
+    private GradeRepository gradeRepository;
 
-	@PostConstruct
-	public void seedDatabase() {
-		if (ymlFileName.isPresent()) {
-			this.deleteAllAndCreateAdmin();
-			try {
-				this.seedDatabase(ymlFileName.get());
-				this.setAvatars();
-			} catch (IOException e) {
-				logger.error("File " + ymlFileName + " doesn't exist or can't be opened");
-			}
-		} else {
-			this.createAdminIfNotExist();
-		}
-	}
+    @Autowired
+    private QuizRepository quizRepository;
 
-	public void seedDatabase(String ymlFileName) throws IOException {
-		assert ymlFileName != null && !ymlFileName.isEmpty();
-		Yaml yamlParser = new Yaml(new Constructor(DatabaseGraph.class));
-		InputStream input = new ClassPathResource(ymlFileName).getInputStream();
-		DatabaseGraph dbGraph = yamlParser.load(input);
+    @Autowired
+    private QuizStudentRepository quizStudentRepository;
 
-		// Save Repositories
-		if (dbGraph.getUserList() != null) {
-			this.userRepository.saveAll(dbGraph.getUserList());
-		}
+    @Autowired
+    private AttendanceRepository attendanceRepository;
 
-		if (dbGraph.getPreferencesList() != null) {
-			this.preferencesRepository.saveAll(dbGraph.getPreferencesList());
-		}
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-		if (dbGraph.getCoursesList() != null) {
-			this.courseRepository.saveAll(dbGraph.getCoursesList());
-		}
+    @Autowired
+    private Environment environment;
 
-		if (dbGraph.getSubjectsList() != null) {
-			this.subjectRepository.saveAll(dbGraph.getSubjectsList());
-		}
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseSeederService.class);
 
-		if (dbGraph.getQuizesList() != null) {
-			this.quizRepository.saveAll(dbGraph.getQuizesList());
-		}
-		
-		if (dbGraph.getQuizesStudentList() != null) {
-			this.quizStudentRepository.saveAll(dbGraph.getQuizesStudentList());
-		}
-		
-		if (dbGraph.getEvaluationsList() != null) {
-			this.evaluationRepository.saveAll(dbGraph.getEvaluationsList());
-		}
-		
-		if (dbGraph.getGradesList() != null) {
-			this.gradeRepository.saveAll(dbGraph.getGradesList());
-		}
-		
-		if (dbGraph.getAttendancesList() != null) {
-			this.attendanceRepository.saveAll(dbGraph.getAttendancesList());
-		}
+    @PostConstruct
+    public void seedDatabase() {
+        String[] profiles = this.environment.getActiveProfiles();
+        if (Arrays.asList(profiles).contains("dev")) {
+            if (ymlFileName.isPresent()) {
+                this.deleteAllAndCreateAdmin();
+                try {
+                    this.seedDatabase(ymlFileName.get());
+                    this.setAvatars();
+                } catch (IOException e) {
+                    logger.error("File " + ymlFileName + " doesn't exist or can't be opened");
+                }
+            }
 
-		logger.warn("------------------------- Seed: " + ymlFileName + "-----------");
-	}
+        } else if (Arrays.asList(profiles).contains("prod")) {
+            this.createAdminIfNotExist();
+        }
 
-	public void deleteAllAndCreateAdmin() {
-		logger.warn("------------------------- delete DB and Create Admin-----------");
-		mongoTemplate.getDb().drop();
-		this.createAdminIfNotExist();
-	}
+    }
 
-	public void createAdminIfNotExist() {
-		if (this.userRepository.findByUsername(this.username) == null) {
-			User user = new User(this.username, this.password);
-			user.setFirstName("Daniel Jesús");
-			user.setLastName("Rubio Parra");
-			user.setGender(Gender.HOMBRE);
-			user.setAvatar(new Avatar("admin.png", "image/png", Avatar.SERVER_AVATAR_PATH + "admin.png"));
-			user.setRoles(new Role[] { Role.ADMIN });
-			user.setCommune(Commune.QUINTA_NORMAL);
-			this.userRepository.save(user);
+    public void seedDatabase(String ymlFileName) throws IOException {
+        assert ymlFileName != null && !ymlFileName.isEmpty();
+        Yaml yamlParser = new Yaml(new Constructor(DatabaseGraph.class));
+        InputStream input = new ClassPathResource(ymlFileName).getInputStream();
+        DatabaseGraph dbGraph = yamlParser.load(input);
 
-			Theme theme = new Theme("indigo-pink-dark", true, "#3F51B5");
-			Preferences preference = new Preferences(user, theme);
-			this.preferencesRepository.insert(preference);
-		}
-	}
+        if (dbGraph.getUserList() != null) {
+            this.userRepository.saveAll(dbGraph.getUserList());
+        }
 
-	public void setAvatars() {
-		logger.warn("------------------------- setting avatars-----------");
-		List<User> users = userRepository.findAll();
-		Iterator<User> it = users.iterator();
-		while (it.hasNext()) {
-			User user = it.next();
-			if (user.getAvatar() != null) {
-				String path = Avatar.SERVER_AVATAR_PATH + user.getAvatar().getName();
-				String avatarBase64 = NX_UtilBase64Image.encoder(path);
-				user.getAvatar().setData(avatarBase64);
-                userRepository.save(user); 
-			}
-		}
-		
-	}
+        if (dbGraph.getPreferencesList() != null) {
+            this.preferencesRepository.saveAll(dbGraph.getPreferencesList());
+        }
+
+        if (dbGraph.getCoursesList() != null) {
+            this.courseRepository.saveAll(dbGraph.getCoursesList());
+        }
+
+        if (dbGraph.getSubjectsList() != null) {
+            this.subjectRepository.saveAll(dbGraph.getSubjectsList());
+        }
+
+        if (dbGraph.getQuizesList() != null) {
+            this.quizRepository.saveAll(dbGraph.getQuizesList());
+        }
+
+        if (dbGraph.getQuizesStudentList() != null) {
+            this.quizStudentRepository.saveAll(dbGraph.getQuizesStudentList());
+        }
+
+        if (dbGraph.getEvaluationsList() != null) {
+            this.evaluationRepository.saveAll(dbGraph.getEvaluationsList());
+        }
+
+        if (dbGraph.getGradesList() != null) {
+            this.gradeRepository.saveAll(dbGraph.getGradesList());
+        }
+
+        if (dbGraph.getAttendancesList() != null) {
+            this.attendanceRepository.saveAll(dbGraph.getAttendancesList());
+        }
+
+        logger.warn("------------------------- Seed: " + ymlFileName + "-----------");
+    }
+
+    public void deleteAllAndCreateAdmin() {
+        logger.warn("------------------------- delete DB and Create Admin-----------");
+        mongoTemplate.getDb().drop();
+        this.createAdminIfNotExist();
+    }
+
+    public void createAdminIfNotExist() {
+        if (this.userRepository.findByUsername(this.username) == null) {
+            User user = new User(this.username, this.password);
+            user.setFirstName("Daniel Jesús");
+            user.setLastName("Rubio Parra");
+            user.setGender(Gender.HOMBRE);
+            user.setAvatar(new Avatar("admin.png", "image/png", Avatar.SERVER_AVATAR_PATH + "admin.png"));
+            user.setRoles(new Role[]{Role.ADMIN});
+            user.setCommune(Commune.QUINTA_NORMAL);
+            this.userRepository.save(user);
+
+            Theme theme = new Theme("indigo-pink-dark", true, "#3F51B5");
+            Preferences preference = new Preferences(user, theme);
+            this.preferencesRepository.insert(preference);
+        }
+    }
+
+    public void setAvatars() {
+        logger.warn("------------------------- setting avatars-----------");
+        List<User> users = userRepository.findAll();
+        Iterator<User> it = users.iterator();
+        while (it.hasNext()) {
+            User user = it.next();
+            if (user.getAvatar() != null) {
+                String path = Avatar.SERVER_AVATAR_PATH + user.getAvatar().getName();
+                String avatarBase64 = NX_UtilBase64Image.encoder(path);
+                user.getAvatar().setData(avatarBase64);
+                userRepository.save(user);
+            }
+        }
+
+    }
 
 }
