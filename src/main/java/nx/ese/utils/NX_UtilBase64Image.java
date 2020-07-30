30a5cc862625d43ca5c9c4e99a75daceb0f788ea
Daniels <d.rubio@outlook.com>
2020-07-30 12:40:48 -0400

    logs

--- src/main/java/nx/ese/utils/NX_UtilBase64Image.java
+++ src/main/java/nx/ese/utils/NX_UtilBase64Image.java
@@ -1,5 +1,8 @@
 package nx.ese.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,30 +11,33 @@ import java.io.IOException;
 import java.util.Base64;
 
 public class NX_UtilBase64Image {
-	public static String encoder(String imagePath) {
-		File file = new File(imagePath);
-		try (FileInputStream imageInFile = new FileInputStream(file)) {
-			String base64Image = "";
-			byte[] imageData = new byte[(int) file.length()];
-			imageInFile.read(imageData);
-			base64Image = Base64.getEncoder().encodeToString(imageData);
-			return base64Image;
-		} catch (FileNotFoundException e) {
-			System.out.println("Image not found" + e);
-		} catch (IOException ioe) {
-			System.out.println("Exception while reading the Image " + ioe);
-		}
-		return null;
-	}
 
-	public static void decoder(String base64Image, String pathFile) {
-		try (FileOutputStream imageOutFile = new FileOutputStream(pathFile)) {
-			byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
-			imageOutFile.write(imageByteArray);
-		} catch (FileNotFoundException e) {
-			System.out.println("Image not found" + e);
-		} catch (IOException ioe) {
-			System.out.println("Exception while reading the Image " + ioe);
-		}
-	}
+    private static final Logger logger = LoggerFactory.getLogger(NX_UtilBase64Image.class);
+
+    public static String encoder(String imagePath) {
+        File file = new File(imagePath);
+        try (FileInputStream imageInFile = new FileInputStream(file)) {
+            String base64Image = "";
+            byte[] imageData = new byte[(int) file.length()];
+            imageInFile.read(imageData);
+            base64Image = Base64.getEncoder().encodeToString(imageData);
+            return base64Image;
+        } catch (FileNotFoundException e) {
+            logger.error("Image not found {}", e.getMessage());
+        } catch (IOException ioe) {
+            logger.error("Exception while reading the Image {}", ioe.getMessage());
+        }
+        return null;
+    }
+
+    public static void decoder(String base64Image, String pathFile) {
+        try (FileOutputStream imageOutFile = new FileOutputStream(pathFile)) {
+            byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
+            imageOutFile.write(imageByteArray);
+        } catch (FileNotFoundException e) {
+            logger.error("Image not found {}", e.getMessage());
+        } catch (IOException ioe) {
+            logger.error("Exception while reading the Image {}", ioe.getMessage());
+        }
+    }
 }
